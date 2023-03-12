423164879b256fc37af340bffa964fcf7cda500e
Justice Essien <essienjustice@gmail.com>
2023-03-12 15:06:30 +0100

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { signUpUser, loginUser } = require("../controllers/loginController");
-// const { auth, isAdmin } = require("../middleware/protect");
 
 router.post("/signup", signUpUser);
 router.post("/login", loginUser);
